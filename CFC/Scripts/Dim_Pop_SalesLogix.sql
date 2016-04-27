ac25023db80acc3fa2dd9603ad79a80e16639113
appstrats <anu@applicationstrategies.com>
2016-04-27 07:23:22 -0700

    Latest version

--- CFC/Scripts/Dim_Pop_SalesLogix.sql
+++ CFC/Scripts/Dim_Pop_SalesLogix.sql
@@ -2,20 +2,27 @@ use [CFC_DW]
 GO
 --Account
 -- exec sp_pop_account_SalesLogix 
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[sp_pop_account_SalesLogix]'))
+DROP PROC [dbo].sp_pop_account_SalesLogix
+go
 Create PROC sp_pop_account_SalesLogix  as
 begin
 set nocount on
 
-insert into [CFC_DW].sysdba.Account(ACCOUNTID_key,ACCOUNTID,ACCOUNT)
-select distinct isnull((select max(ACCOUNTID_key) from [CFC_DW].sysdba.ACCOUNT where ACCOUNTID_key>-1),0) +
+insert into Account(ACCOUNTID_key,ACCOUNTID,ACCOUNT)
+select distinct isnull((select max(ACCOUNTID_key) from ACCOUNT where ACCOUNTID_key>-1),0) +
 ROW_NUMBER() over (ORDER BY s.ACCOUNTID ), s.ACCOUNTID, s.ACCOUNT 
 from SalesLogix.sysdba.ACCOUNT s
-left outer join [CFC_DW].sysdba.Account d on s.ACCOUNTID  = d.ACCOUNTID
+left outer join Account d on s.ACCOUNTID  = d.ACCOUNTID
 where d.ACCOUNTID is null
 end;
 
+go
 --Region
 -- exec sp_pop_region_SalesLogix 
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[sp_pop_region_SalesLogix]'))
+DROP PROC [dbo].sp_pop_region_SalesLogix
+go
 Create PROC sp_pop_region_SalesLogix as
 begin
 set nocount on
@@ -23,11 +30,11 @@ set nocount on
 with T_Label_region as
 (select distinct(REGION) rg  from SalesLogix.sysdba.ACCOUNT )
 
-insert into [CFC_DW].sysdba.Region(Region_key,Region)
-select distinct isnull((select max(Region_key) from [CFC_DW].sysdba.Ship_Region where Region_key>-1),0) +
+insert into Region(Region_key,Region)
+select distinct isnull((select max(Region_key) from Ship_Region where Region_key>-1),0) +
 ROW_NUMBER() over (ORDER BY s.rg ), s.rg 
 from T_Label_region s
-left outer join [CFC_DW].sysdba.Ship_Region d on s.rg  = d.Region
+left outer join Ship_Region d on s.rg  = d.Region
 where d.Region is null
 
 end;
@@ -38,15 +45,18 @@ use [CFC_DW]
 GO
 --Product
 -- exec sp_pop_procuct_SalesLogix 
-Create PROC sp_pop_procuct_SalesLogix as
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[sp_pop_product_SalesLogix]'))
+DROP PROC [dbo].sp_pop_product_SalesLogix
+go
+Create PROC sp_pop_product_SalesLogix as
 begin
 set nocount on
 
-insert into [CFC_DW].sysdba.Product(PRODUCT_NUMBER_key,PRODUCT_Family,PRODUCT_NUMBER,PRODUCT_NAME)
-select distinct isnull((select max(PRODUCT_NUMBER_key) from [CFC_DW].sysdba.Product where PRODUCT_NUMBER_key>-1),0) +
+insert into Product(PRODUCT_NUMBER_key,PRODUCT_Family,PRODUCT_NUMBER,PRODUCT_NAME)
+select distinct isnull((select max(PRODUCT_NUMBER_key) from Product where PRODUCT_NUMBER_key>-1),0) +
 ROW_NUMBER() over (ORDER BY s.PRODUCTID ),s.FAMILY, s.PRODUCTID ,s.NAME
 from SalesLogix.sysdba.PRODUCT s
-left outer join [CFC_DW].sysdba.PRODUCT d on s.PRODUCTID  = d.PRODUCT_NUMBER
+left outer join PRODUCT d on s.PRODUCTID  = d.PRODUCT_NUMBER
 where d.PRODUCT_NUMBER is null
 
 end;
@@ -54,6 +64,9 @@ end;
 GO
 --Order type
 -- exec sp_pop_ordertype_SalesLogix 
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[sp_pop_ordertype_SalesLogix]'))
+DROP PROC [dbo].sp_pop_ordertype_SalesLogix
+go
 Create PROC sp_pop_ordertype_SalesLogix  as
 begin
 set nocount on
@@ -65,7 +78,7 @@ insert into [CFC_DW].[sysdba].[Order_Type]([Order_Type_Key],[Order_Type])
 select distinct isnull((select max([Order_Type_Key]) from [CFC_DW].[sysdba].[Order_Type] where [Order_Type_Key]>-1),0) +
 ROW_NUMBER() over (ORDER BY s.ot ), s.ot
 from T_Label_Order_Type s
-left outer join [CFC_DW].sysdba.[Order_Type] d on s.ot  = d.[Order_Type]
+left outer join [Order_Type] d on s.ot  = d.[Order_Type]
 where d.[Order_Type] is null
 end;
 
@@ -74,6 +87,9 @@ end;
 GO
 --Order_Method
 -- exec sp_pop_Order_Method_SalesLogix 
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[sp_pop_Order_Method_SalesLogix]'))
+DROP PROC [dbo].sp_pop_Order_Method_SalesLogix
+go
 Create PROC sp_pop_Order_Method_SalesLogix  as
 begin
 set nocount on
@@ -85,7 +101,7 @@ insert into [CFC_DW].[sysdba].[Order_Method]([Order_Method_Key],[Order_Method])
 select distinct isnull((select max([Order_Method_Key]) from [CFC_DW].[sysdba].[Order_Method] where [Order_Method_Key]>-1),0) +
 ROW_NUMBER() over (ORDER BY s.om ), s.om
 from T_Label_Order_Method s
-left outer join [CFC_DW].sysdba.[Order_Method] d on s.om  = d.[Order_Method]
+left outer join [Order_Method] d on s.om  = d.[Order_Method]
 where d.[Order_Method] is null
 end;
 
@@ -93,7 +109,10 @@ go
 
 --MDR_file_Type
 -- exec sp_pop_MDR_file_Type_SalesLogix 
-Create PROC sp_pop_MDR_file_Type_SalesLogix  as
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[sp_pop_MDR_file_Type_SalesLogix]'))
+DROP PROC [dbo].sp_pop_MDR_file_Type_SalesLogix
+go
+Create  PROC sp_pop_MDR_file_Type_SalesLogix  as
 begin
 set nocount on
 
@@ -104,7 +123,7 @@ insert into [CFC_DW].[sysdba].[MDR_file_Type]([MDR_file_Type_Key],[MDR_file_Type
 select distinct isnull((select max([MDR_file_Type_Key]) from [CFC_DW].[sysdba].[MDR_file_Type] where [MDR_file_Type_Key]>-1),0) +
 ROW_NUMBER() over (ORDER BY s.mf ), s.mf
 from T_Label_MDR_file_Type s
-left outer join [CFC_DW].sysdba.[MDR_file_Type] d on s.mf  = d.[MDR_file_Type]
+left outer join [MDR_file_Type] d on s.mf  = d.[MDR_file_Type]
 where d.[MDR_file_Type] is null
 end;
 
@@ -112,15 +131,18 @@ end;
 GO
 --User
 -- exec sp_pop_User_SalesLogix 
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[sp_pop_User_SalesLogix]'))
+DROP PROC [dbo].sp_pop_User_SalesLogix
+go
 Create PROC sp_pop_User_SalesLogix as
 begin
 set nocount on
 
-insert into [CFC_DW].sysdba.User(Name_key,Name,User2)
-select distinct isnull((select max(Name_key) from [CFC_DW].sysdba.User where Name_key>-1),0) +
+insert into [User](Name_key,Name,User2)
+select distinct isnull((select max(Name_key) from [User] where Name_key>-1),0) +
 ROW_NUMBER() over (ORDER BY s.USERFIELD1 ),s.USERFIELD1, s.USERFIELD2
-from SalesLogix.sysdba.PRODUCT s
-left outer join [CFC_DW].sysdba.User d on s.USERFIELD1  = d.Name
+from SalesLogix.sysdba.USERINFO s
+left outer join [User] d on s.USERFIELD1  = d.Name
 where d.Name is null
 
 end;
@@ -128,7 +150,10 @@ end;
 GO
 --Funding
 -- exec sp_pop_Funding_SalesLogix 
-Create PROC sp_pop_Funding_SalesLogix  as
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[sp_pop_Funding_SalesLogix]'))
+DROP PROC [dbo].sp_pop_Funding_SalesLogix
+go
+Create  PROC sp_pop_Funding_SalesLogix  as
 begin
 set nocount on
 
@@ -139,7 +164,7 @@ insert into [CFC_DW].[sysdba].[Funding]([Funding_Key],[Funding])
 select distinct isnull((select max([Funding_Key]) from [CFC_DW].[sysdba].[Funding] where [Funding_Key]>-1),0) +
 ROW_NUMBER() over (ORDER BY s.fd ), s.fd
 from T_Label_Funding s
-left outer join [CFC_DW].sysdba.[Funding] d on s.fd  = d.[Funding]
+left outer join [Funding] d on s.fd  = d.[Funding]
 where d.[Funding] is null
 end;
 
@@ -147,6 +172,9 @@ end;
 GO
 --address
 -- exec sp_pop_address_SalesLogix 
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[sp_pop_address_SalesLogix]'))
+DROP PROC [dbo].sp_pop_address_SalesLogix
+go
 Create PROC sp_pop_address_SalesLogix  as
 begin
 set nocount on
@@ -158,13 +186,16 @@ insert into [CFC_DW].[sysdba].[Address]([Addr1_Key],[Addr1],[Addr2])
 select distinct isnull((select max([Addr1_Key]) from [CFC_DW].[sysdba].Address where [Addr1_Key]>-1),0) +
 ROW_NUMBER() over (ORDER BY s.ad ), s.ad, s.ADDRESS2
 from T_Label_Funding s
-left outer join [CFC_DW].sysdba.[Address] d on s.Ad  = d.[Addr1]
+left outer join [Address] d on s.Ad  = d.[Addr1]
 where d.[Addr1] is null
 end;
 
 GO
 --location
 -- exec sp_pop_location_SalesLogix 
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[sp_pop_location_SalesLogix]'))
+DROP PROC [dbo].sp_pop_location_SalesLogix
+go
 Create PROC sp_pop_location_SalesLogix  as
 begin
 set nocount on
@@ -176,7 +207,7 @@ insert into [CFC_DW].[sysdba].[Location]([Location_Key],[POSTALCODE],[CITY],[STA
 select distinct isnull((select max([Location_Key]) from [CFC_DW].[sysdba].[Location] where [Location_Key]>-1),0) +
 ROW_NUMBER() over (ORDER BY s.pc ), s.POSTALCODE, s.CITY, s.STATE, s.COUNTRY
 from T_Label_Funding s
-left outer join [CFC_DW].sysdba.[Location] d on s.pc  = d.[POSTALCODE]
+left outer join [Location] d on s.pc  = d.[POSTALCODE]
 where d.[POSTALCODE] is null
 end;
 
@@ -184,15 +215,18 @@ end;
 GO
 --Shipper
 -- exec sp_pop_Shipper_SalesLogix 
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[sp_pop_Shipper_SalesLogix]'))
+DROP PROC [dbo].sp_pop_Shipper_SalesLogix
+go
 Create PROC sp_pop_Shipper_SalesLogix as
 begin
 set nocount on
 
-insert into [CFC_DW].sysdba.Shipper(SHIPPERID_key,ShipName,SHIPPERID)
-select distinct isnull((select max(SHIPPERID_key) from [CFC_DW].sysdba.Shipper where SHIPPERID_key>-1),0) +
+insert into Shipper(SHIPPER_key,ShipName,SHIPPERID)
+select distinct isnull((select max(SHIPPER_key) from Shipper where SHIPPER_key>-1),0) +
 ROW_NUMBER() over (ORDER BY s.SHIPPERID ),'unknown', s.SHIPPERID
 from SalesLogix.sysdba.salesorder_ext s
-left outer join [CFC_DW].sysdba.Shipper d on s.SHIPPERID  = d.SHIPPERID
+left outer join Shipper d on s.SHIPPERID  = d.SHIPPERID
 where d.SHIPPERID is null
 
 end;
