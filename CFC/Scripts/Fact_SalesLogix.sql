d6d37fc8245b8486dfaa14eb77eae5f9a1c5806d
leojoseph75 <leojoseph_75@yahoo.com>
2016-05-01 12:51:33 +0530

    Tested for loading and fixed few errors

--- CFC/Scripts/Fact_SalesLogix.sql
+++ CFC/Scripts/Fact_SalesLogix.sql
@@ -1,8 +1,10 @@
-create PROC sp_populate_SalesLogix_fact  as
+--exec sp_populate_fact 
+--drop proc sp_populate_fact  
+create PROC sp_populate_fact  as
 begin
 set nocount on
 
-
+-- SalesLogx Data
 SELECT       
 CAST(CASE WHEN month(Salesorder.shippeddate) IN (1, 2, 3) THEN year(Salesorder.shippeddate) - 1
 ELSE year(Salesorder.shippeddate) END AS varchar(4))
@@ -11,62 +13,62 @@ CAST(CASE WHEN month(Salesorder.shippeddate) IN (4, 5, 6, 7, 8, 9, 10, 11, 12) T
 ELSE year(Salesorder.shippeddate) END AS varchar(4))
 AS [Fiscal_Year],
  
-CASE WHEN sysdba.salesorder_ext.FUNDING IS NULL OR sysdba.salesorder_ext.FUNDING = 'unknown' THEN 'Unknown' ELSE sysdba.salesorder_ext.FUNDING END AS Funding,
-sysdba.salesorder_ext.ORDER_METHOD,
-sysdba.salesorder_ext.SHIPPERID,
-sysdba.SALESORDER.ORDERTYPE,
-convert(varchar(8),sysdba.SALESORDER.SHIPPEDDATE, 112) SHIPPEDDATE,
-convert(varchar(8),sysdba.SALESORDER.ORDERDATE,112) ORDERDATE,
-sysdba.ACCOUNT.ACCOUNTID,
-sysdba.ACCOUNT.PARENTID,
-right(sysdba.ACCOUNT.ACCOUNTID,8) as CFCID,
-sysdba.SALESORDER.INVOICETOTAL as SO_TFI,
-sysdba.SALESORDER.INVOICETOTAL - sysdba.SALESORDER.FREIGHT - sysdba.SALESORDER.TAX AS SO_NET,
-sysdba.SALESORDERDETAIL.QUANTITYSHIPPED as QTY,
-sysdba.SALESORDERDETAIL.EXTENDEDPRICEINVOICED as PRODUCT_EXTENDEDPRICEINVOICED,
+CASE WHEN salesorder_ext.FUNDING IS NULL OR salesorder_ext.FUNDING = 'unknown' THEN 'Unknown' ELSE salesorder_ext.FUNDING END AS Funding,
+salesorder_ext.ORDER_METHOD,
+salesorder_ext.SHIPPERID,
+SALESORDER.ORDERTYPE,
+convert(varchar(8),SALESORDER.SHIPPEDDATE, 112) SHIPPEDDATE,
+convert(varchar(8),SALESORDER.ORDERDATE,112) ORDERDATE,
+ACCOUNT.ACCOUNTID,
+ACCOUNT.PARENTID,
+right(ACCOUNT.ACCOUNTID,8) as CFCID,
+SALESORDER.INVOICETOTAL as SO_TFI,
+SALESORDER.INVOICETOTAL - SALESORDER.FREIGHT - SALESORDER.TAX AS SO_NET,
+SALESORDERDETAIL.QUANTITYSHIPPED as QTY,
+SALESORDERDETAIL.EXTENDEDPRICEINVOICED as PRODUCT_EXTENDEDPRICEINVOICED,
 CASE
-WHEN sysdba.product.ACTUALID in  ('000901', '000900', '000902','000903') then 'Suite'
-when sysdba.product.ACTUALID like'disc%' OR sysdba.product.ACTUALID like'cpn%' or sysdba.product.ACTUALID like'TE%'then 'Discount'
-WHEN sysdba.product.ACTUALID in  ('SSLI') then 'SSLI'
-WHEN sysdba.product.ACTUALID in  ('WORKSHOP') then 'IMPL'
-WHEN sysdba.product.ACTUALID in  ('NONRTN') then 'NONRTN'
-when sysdba.product.ACTUALID like 'IMPSVC%' THEN 'IMPSVC'
-ELSE sysdba.product.family
+WHEN product.ACTUALID in  ('000901', '000900', '000902','000903') then 'Suite'
+when product.ACTUALID like'disc%' OR product.ACTUALID like'cpn%' or product.ACTUALID like'TE%'then 'Discount'
+WHEN product.ACTUALID in  ('SSLI') then 'SSLI'
+WHEN product.ACTUALID in  ('WORKSHOP') then 'IMPL'
+WHEN product.ACTUALID in  ('NONRTN') then 'NONRTN'
+when product.ACTUALID like 'IMPSVC%' THEN 'IMPSVC'
+ELSE product.family
 end
 as PRODUCT_Family,
-sysdba.PRODUCT.ACTUALID as PRODUCT_NUMBER,
-sysdba.PRODUCT.NAME as PRODUCT_NAME,
+PRODUCT.ACTUALID as PRODUCT_NUMBER,
+PRODUCT.NAME as PRODUCT_NAME,
 case when account.parentid is null then account.ACCOUNT else ACCOUNT_1.Account end as DistrictOrNoParent,
-sysdba.ACCOUNT.ACCOUNT,
---sysdba.ACCOUNT.TYPE,
---sysdba.ACCOUNT.STATUS,
-sysdba.ACCOUNT.REGION,
-SYSDBA.ADDRESS.CITY,
-sysdba.ADDRESS.STATE,
-sysdba.ADDRESS.COUNTRY,
-sysdba.ADDRESS.POSTALCODE
---sysdba.ACCOUNTFFEXT.MDR_FILETYPE, 
---sysdba.ACCOUNTFFEXT.MDR_ENROLLMENT,
---sysdba.ACCOUNTFFEXT.R_RANK_ENROLL
+ACCOUNT.ACCOUNT,
+--ACCOUNT.TYPE,
+--ACCOUNT.STATUS,
+ACCOUNT.REGION,
+ADDRESS.CITY,
+ADDRESS.STATE,
+ADDRESS.COUNTRY,
+ADDRESS.POSTALCODE
+--ACCOUNTFFEXT.MDR_FILETYPE, 
+--ACCOUNTFFEXT.MDR_ENROLLMENT,
+--ACCOUNTFFEXT.R_RANK_ENROLL
 into #sl_fact
 FROM           
-sysdba.SALESORDER
-Left JOIN  sysdba.SALESORDER_EXT ON sysdba.SALESORDER.SALESORDERID = sysdba.SALESORDER_EXT.SALESORDERID
-Left JOIN sysdba.SALESORDERDETAIL ON sysdba.SALESORDER.SALESORDERID = sysdba.SALESORDERDETAIL.SALESORDERID
-left JOIN sysdba.PRODUCT ON sysdba.SALESORDERDETAIL.PRODUCTID = sysdba.PRODUCT.PRODUCTID
-Left join sysdba.ACCOUNT ON sysdba.SALESORDER.ACCOUNTID = sysdba.ACCOUNT.ACCOUNTID
-INNER JOIN sysdba.ADDRESS ON sysdba.ACCOUNT.ADDRESSID = sysdba.ADDRESS.ADDRESSID
-inner JOIN sysdba.ACCOUNTFFEXT ON sysdba.ACCOUNT.ACCOUNTID = sysdba.ACCOUNTFFEXT.ACCOUNTID
-left join sysdba.ACCOUNT AS ACCOUNT_1 ON ACCOUNT.PARENTID = ACCOUNT_1.ACCOUNTID
+SalesLogix.sysdba.SALESORDER
+Left JOIN  SalesLogix.sysdba.SALESORDER_EXT ON SALESORDER.SALESORDERID = SALESORDER_EXT.SALESORDERID
+Left JOIN SalesLogix.sysdba.SALESORDERDETAIL ON SALESORDER.SALESORDERID = SALESORDERDETAIL.SALESORDERID
+left JOIN SalesLogix.sysdba.PRODUCT ON SALESORDERDETAIL.PRODUCTID = PRODUCT.PRODUCTID
+Left join SalesLogix.sysdba.ACCOUNT ON SALESORDER.ACCOUNTID = ACCOUNT.ACCOUNTID
+INNER JOIN SalesLogix.sysdba.ADDRESS ON ACCOUNT.ADDRESSID = ADDRESS.ADDRESSID
+inner JOIN SalesLogix.sysdba.ACCOUNTFFEXT ON ACCOUNT.ACCOUNTID = ACCOUNTFFEXT.ACCOUNTID
+left join SalesLogix.sysdba.ACCOUNT AS ACCOUNT_1 ON ACCOUNT.PARENTID = ACCOUNT_1.ACCOUNTID
 WHERE       
-(sysdba.SALESORDERDETAIL.EXTENDEDPRICEINVOICED <> '0') AND
-(sysdba.SALESORDERDETAIL.QUANTITYSHIPPED <> 0)
-and sysdba.SALESORDER.SHIPPEDDATE >= DATEADD(year, - 5, GETDATE())
+(SALESORDERDETAIL.EXTENDEDPRICEINVOICED <> '0') AND
+(SALESORDERDETAIL.QUANTITYSHIPPED <> 0)
+and SALESORDER.SHIPPEDDATE >= DATEADD(year, - 5, GETDATE())
 
 
 
 select isnull((select max(cfc_key) from [CFC_DW].dbo.CFC_fact),0) + 
-ROW_NUMBER() over (ORDER BY T_fact.ACCOUNTID) cfc_key,
+ROW_NUMBER() over (ORDER BY f.CFCID) cfc_key,
 -1 Ship_Location_Key,
 -1 Ship_Address_Key,
 -1 Order_Type_Key,
@@ -82,9 +84,9 @@ isnull(Account_key, -1) Account_Key,
 -1 Bill_Address_Key,
 -1 Bill_Location_Key,
 isnull(ship_d.date_key,-1) Ship_Date_Key,
-Bill_Date_Key,
+-1 Bill_Date_Key,
 isnull(ord_d.date_key,-1) Order_Date_Key,
-f.QTY, Qty_Shiped,
+f.QTY Qty_Shiped,
 0 Extended_Price
 
   into #sl_fact_dw
@@ -95,10 +97,13 @@ f.QTY, Qty_Shiped,
   left join Shipper s on f.SHIPPERID = s.SHIPPERID
   left join Product p on f.PRODUCT_NUMBER = p.PRODUCT_NUMBER
   left join Account ACT on f.ACCOUNT = ACT.ACCOUNT
-  left join Dim_Date ship_d on f.SHIPPEDDATE = d.date_key
+  left join Dim_Date ship_d on f.SHIPPEDDATE = ship_d.date_key
   left join Dim_Date ord_d on f.ORDERDATE = ord_d.date_key    
   
-     insert into CFC_DW.dbo.CFC_fact(
+
+  truncate table cfc_fact;
+
+   insert into CFC_DW.dbo.CFC_fact(
 	cfc_key,
 	Ship_Location_Key,
 	Ship_Address_Key,
@@ -143,5 +148,8 @@ f.QTY, Qty_Shiped,
 	Extended_Price
 
 	from #sl_fact_dw
+	
+	
+
 	
   end;
