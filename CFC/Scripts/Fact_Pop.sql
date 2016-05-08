0708d25c878fcc99385a51b1597e7667577c4981
philipmilton <philipmilton.paulraj@gmail.com>
2016-05-08 07:59:15 +0530

    updated fact proc 8-5-16

--- CFC/Scripts/Fact_Pop.sql
+++ CFC/Scripts/Fact_Pop.sql
@@ -58,6 +58,64 @@ ACCOUNTFFEXT.MDR_FILETYPE,
 --ACCOUNTFFEXT.R_RANK_ENROLL
 SALESORDER.FFShipto_Zip shippostcode
 
+UNION
+ 
+select
+--CAST(CASE WHEN ARDocType='IN' and  month(ShipDateAct) IN (1, 2, 3) THEN year(ShipDateAct) - 1
+--ELSE
+--year(ShipDateAct) END AS varchar(4))
+--+ '-' +
+--CAST(CASE WHEN ARDocType='IN' and  month(ShipDateAct) IN (4, 5, 6, 7, 8, 9, 10, 11, 12) THEN year(ShipDateAct) + 1
+--ELSE year(ShipDateAct) END AS varchar(4))
+CAST(CASE WHEN  month(soh.OrdDate) IN (1, 2, 3) THEN year(soh.OrdDate) - 1 ELSE year(soh.OrdDate) END AS varchar(4))
++ '-' +
+CAST(CASE WHEN  month(soh.OrdDate) IN (4, 5, 6, 7, 8, 9, 10, 11, 12) THEN year(soh.OrdDate) + 1 ELSE year(soh.OrdDate) End AS varchar(4))
+AS [Fiscal Year],
+Case when soh.User4='' or soh.user4='unknown' then 'Unknown' else soh.user4 end as Funding,
+soh.User3 as ORDER_METHOD,
+SOH.SHIPPERID,
+SOH.SOTYPEID AS ORDERTYPE,
+ShipDateAct,
+soh.OrdDate,
+(select ltrim(rtrim(cu.User1)) from [CFCAPP].dbo.[Customer] as cu where cu.custid=soh.custid) as ACCOUNTID,
+(SELECT PARENTID FROM SalesLogix.SYSDBA.ACCOUNT AS A WHERE A.EXTERNALACCOUNTNO=SOH.CustID) AS PARENTID,
+(select right(ltrim(rtrim(cu.User1)),8) as CFC_ID from [CFCAPP].dbo.[Customer] as cu where cu.custid=soh.custid) as CFCID,
+soh.CuryTotInvc as SO_TFI,
+soh.CuryTotInvc-TotFrtCost-TotTax as SO_NET,
+sol.QtyShip as QTY,
+sol.CuryTotInvc as PRODUCT_EXTENDEDPRICEINVOICED,
+Case when sol.ItemGLClassID  like 'SS%' then 'Second Step' when sol.ItemGLClassID  like 'bp%' then 'BPU' when sol.ItemGLClassID  like 'cp%' then 'CPU'
+when sol.InvtID like'disc%' OR sol.InvtID like'cpn%' or sol.InvtID like'TE%' then 'Discount'
+when sol.InvtID in  ('000901', '000900', '000902','000903') then 'Suite'
+when sol.ItemGLClassID  like 'SR%' then 'Steps To Respect'
+else sol.ItemGLClassID
+end AS PRODUCT_FAMILY,
+SOL.InvtID AS PRODUCT_NUMBER,
+SOL.Descr,
+(SELECT
+case when a.parentid is null then a.ACCOUNT else ACCOUNT_1.Account end as DistrictOrNoParent
+--ACCOUNT_1.account
+FROM SalesLogix.SYSDBA.ACCOUNT AS A
+left join sysdba.ACCOUNT AS ACCOUNT_1 ON A.PARENTID = ACCOUNT_1.ACCOUNTID
+WHERE A.EXTERNALACCOUNTNO=SOH.CustID
+) AS DistrictOrNoParent,
+(select cu.Name from [CFCAPP].dbo.[Customer] as cu where cu.custid=soh.custid) as ACCOUNT,
+(SELECT REGION FROM SalesLogix.SYSDBA.ACCOUNT AS A WHERE A.EXTERNALACCOUNTNO=SOH.CustID) AS REGION,
+SOH.ShipCity,
+SOH.ShipState,
+SOH.ShipCountry,
+SOH.ShipZip,
+'' mdrfiletype,
+'' shipzip
+ 
+--orddate, ShipDateAct, soh.ShipperID, SOTypeID, ARDocType, Cancelled,*
+from
+CFCAPP.dbo.SOShipHeader as soh
+left join sysdba.SALESORDER_EXT as so_ext on so_ext.shipperid=soh.ShipperID
+inner join CFCAPP.dbo.SOShipLine as sol on sol.ShipperID=soh.ShipperID 
+where --soh.shipperid='366312' 
+so_ext.SHIPPERID is null and Cancelled = 0 
+
 into #sl_fact
 FROM           
 SalesLogix.sysdba.SALESORDER
@@ -82,7 +140,7 @@ isnull (Location_key,-1) Ship_Location_Key,
 isnull (Order_Type_Key,-1) Order_Type_Key,
 isnull(ORM.Order_Method_Key, -1) Order_Method_Key,
 -1 User_Key,
--1 Ship_Region_Key,
+isnull(Region_Key,-1) Ship_Region_Key,
 isnull(fy.Fiscal_Year_Key, -1) Fiscal_Year_Key,
 isnull(mdrft.MDR_File_Type_Key,-1) MDR_File_Type_Key,
 isnull(s.SHIPPER_Key, -1) Shipper_Key,
@@ -90,12 +148,12 @@ isnull(FND.Funding_Key, -1) Funding_Key,
 isnull(p.Product_Number_Key, -1) Product_Number_Key,
 isnull(Account_key, -1) Account_Key,
 -1 Bill_Address_Key,
--1 Bill_Location_Key,
+isnull (Location_key,-1) Bill_Location_Key,
 isnull(ship_d.date_key,-1) Ship_Date_Key,
 -1 Bill_Date_Key,
 isnull(ord_d.date_key,-1) Order_Date_Key,
 f.QTY Qty_Shiped,
-0 Extended_Price
+f.PRODUCT_EXTENDEDPRICEINVOICED Extended_Price
 
   into #sl_fact_dw
   from #sl_fact f
@@ -109,7 +167,9 @@ f.QTY Qty_Shiped,
   left join Dim_Date ord_d on f.ORDERDATE = ord_d.date_key    
   left join [Order_Type] odr_t on f.ORDERTYPE = odr_t.Order_Type
   left join [Location] sloc on f.shippostcode = sloc.Postcode
+  left join [Location] bloc on f.ShipZip = bloc.Postcode
   left join [MDR_File_Type] mdrft on f.MDR_FILETYPE = mdrft.MDR_File_Type
+  left join Region rgn on f.REGION = s.Region
 
   truncate table cfc_fact;
 
